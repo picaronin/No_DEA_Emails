3582d76f9395a26fb1a4a406667a26bc0124b75d
picaronin <50532828+picaronin@users.noreply.github.com>
2019-06-02 18:38:25 +0200

    Including management through cURL

--- pikaron/nodeaemails/ext.php
+++ pikaron/nodeaemails/ext.php
@@ -29,17 +29,23 @@ class ext extends \phpbb\extension\base
 	{
 		// Requires phpBB 3.2.4 or newer.
 		$config = $this->container->get('config');
-		$is_enableable = phpbb_version_compare($config['version'], '3.2.4', '>=');
+		$lang = $this->container->get('language');
+		$lang->add_lang('nodeaemails', 'pikaron/nodeaemails');
 
 		// Display a custom warning message if requirement fails.
-		if (!$is_enableable)
+		if (!phpbb_version_compare($config['version'], '3.2.4', '>='))
 		{
-			$lang = $this->container->get('language');
-			$lang->add_lang('nodeaemails', 'pikaron/nodeaemails');
-
 			// Suppress the error in case of CLI usage
 			@trigger_error($lang->lang('NO_DEA_EMAILS_INSTALL_ERROR'), E_USER_WARNING);
 		}
-		return $is_enableable;
+
+		// Display a custom warning message if not 'allow_url_fopen' and not extension 'curl' loaded.
+		if (!ini_get('allow_url_fopen') && !extension_loaded('curl'))
+		{
+			// Suppress the error in case of CLI usage
+			@trigger_error($lang->lang('NO_DEA_EMAILS_NO_CURL'), E_USER_WARNING);
+		}
+
+		return true;
 	}
 }
